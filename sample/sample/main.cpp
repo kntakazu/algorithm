eb9bdd77741aa68e5bd3dfa3c1c1b164d49951dd
tanikazunori <kazunoritani04@gmail.com>
2015-03-10 17:01:33 +0900

    first commit

---
+++ sample/sample/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sample
+//
+//  Created by tanikazunori on 2015/03/10.
+//  Copyright (c) 2015年 kazu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
